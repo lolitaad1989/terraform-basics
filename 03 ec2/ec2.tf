60ece1ddfd8542b8b9a273533dbf549c9efac244
lolitaad989 <lolitaad19@gmail.com>
2023-02-14 20:59:35 +0530

    terraform basics

--- 03 ec2/ec2.tf
+++ 03 ec2/ec2.tf
@@ -5,9 +5,10 @@ provider "aws" {
 resource "aws_instance" "my-ec2" {
     ami = "ami-0c1d144c8fdd8d690"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
     tags = {
     Name = "Terraform"
-    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+    
   }
 }
 
