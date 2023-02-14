893049a64e9c33aa6470718ba448a73df88847db
lolitaad989 <lolitaad19@gmail.com>
2023-02-14 20:49:44 +0530

    terraform basics

--- 03 ec2/ec2.tf
+++ 03 ec2/ec2.tf
@@ -13,3 +13,29 @@ Name = "Terraform"
 output "public_dins_output" {
   value = aws_instance.my-ec2.public_dns
 }
+
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_sg"
+  }
+}
