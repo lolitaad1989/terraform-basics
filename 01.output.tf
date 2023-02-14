f1698b2b7604caa59f48fbf11537ece4550c0aa9
lolitaad989 <lolitaad19@gmail.com>
2023-02-14 13:29:49 +0530

    terraform basics

--- 01.output.tf
+++ 01.output.tf
@@ -1,3 +1,15 @@
 output "sample" {
     value = "Hello World" 
 }
+
+variable "demo" {
+  value = "Terraform Training"
+}
+
+output "printing-demo" {
+  value = "Value is ${var.demo}"
+}
+
+output "print-demo" {
+  value = var.demo
+}
