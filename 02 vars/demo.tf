6391fdb40493197b5357e397db1f000166f8e843
lolitaad989 <lolitaad19@gmail.com>
2023-02-14 14:15:05 +0530

    terraform basics

--- 02 vars/demo.tf
+++ 02 vars/demo.tf
@@ -1,6 +1,6 @@
 variable "City" {}
 variable "Town" {} 
 
-output "print city and town name" {
+output "Print city and town name" {
   value = "Name of the city is ${var.City} and the town is ${var.Town}"
 }
