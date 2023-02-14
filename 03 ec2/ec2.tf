c875232a186589ed8882012057463d4d249411e2
lolitaad989 <lolitaad19@gmail.com>
2023-02-14 20:38:16 +0530

    terraform basics

--- 03 ec2/ec2.tf
+++ 03 ec2/ec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "this" {
+resource "aws_instance" "my-ec2" {
 ami = "ami-0c1d144c8fdd8d690"
 instance_type = "t2.micro"
 tags = {
@@ -11,5 +11,5 @@ Name = "Terraform"
 }
 
 output "public_dins_output" {
-  value = aws_instance.this.public_dns
+  value = aws_instance.my-ec2.public_dns
 }
