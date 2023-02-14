de2f116db208812137c4a41f8810463dce1527d9
lolitaad989 <lolitaad19@gmail.com>
2023-02-14 13:40:02 +0530

    terraform basics

--- 01.output.tf
+++ 01.output.tf
@@ -13,3 +13,17 @@ output "printing-demo" {
 output "print-demo" {
   value = var.demo
 }
+
+variable "example-list" {
+  default = [
+    "AWS",
+    "Devops",
+    true,
+    100,
+    "SRE"
+  ]
+}
+
+output "ex-list-op" {
+  value = "Welcome to $(var.example-list[0]) and $(var.example-list[1]) training. Training duration is $(var.example-list[2]) hours and you can also apply for $(var.example-list[3]) jobs"
+}
