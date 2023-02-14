86a80e268994dc79a994114ee64af402dcb19943
lolitaad989 <lolitaad19@gmail.com>
2023-02-14 13:51:50 +0530

    terraform basics

--- 01.output.tf
+++ 01.output.tf
@@ -14,6 +14,7 @@ output "print-demo" {
   value = var.demo
 }
 
+
 variable "example-list" {
   default = [
     "AWS",
@@ -26,4 +27,16 @@ variable "example-list" {
 
 output "ex-list-op" {
   value = "Welcome to ${var.example-list[0]} and ${var.example-list[1]} training. Training duration is ${var.example-list[3]} hours and you can also apply for ${var.example-list[4]} jobs"
+}
+
+variable "ex-map" {
+  default = {
+    Duration = 105,
+    Trainer = "verma",
+    Cloud = "AWS"
+  }
+}
+
+output "ex-map" {
+  value = "Welcome to Devops with ${var.ex-map["Cloud"]}" training and the training duration is ${var.ex-map["Duration"]} and trainer name is ${var.ex-map["Trainer"]}
 }
