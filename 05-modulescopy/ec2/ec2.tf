eb444eee5d83c0c4bea5e2ff53ce2e6a7e78d639
lolitaad989 <lolitaad19@gmail.com>
2023-02-16 00:10:22 +0530

    terraform basics

--- 05-modulescopy/ec2/ec2.tf
+++ 05-modulescopy/ec2/ec2.tf
@@ -8,6 +8,19 @@ resource "aws_instance" "my-ec2" {
     ami = "ami-0c1d144c8fdd8d690"
     instance_type = "t2.micro"
     vpc_security_group_ids = [var.sg]
+  provisioner "remote-exec" {
+  connection {
+    type     = "ssh"
+    user     = "centos"
+    password = "DevOps321"
+    host     = self.public_ip
+  }
+
+    inline = [
+        "ansible-pull -U https://github.com/lolitaad1989/ansible.git -e COMPONENT=mongodb -e ENV=dev robot-pull.yml"
+    ]
+  }
+
   }
 
 
