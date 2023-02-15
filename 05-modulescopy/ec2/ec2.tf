4f5d324cb4373ba11628a56683996ea4d6f31c8f
lolitaad989 <lolitaad19@gmail.com>
2023-02-16 01:18:09 +0530

    terraform basics

--- 05-modulescopy/ec2/ec2.tf
+++ 05-modulescopy/ec2/ec2.tf
@@ -1,29 +1,29 @@
-# creates ec2 instance
-resource "aws_instance" "my-ec2" {
-  ami                     = "ami-0c1d144c8fdd8d690"
-  instance_type           = "t2.micro"
-  vpc_security_group_ids  = [var.sg]
+# # creates ec2 instance
+# resource "aws_instance" "my-ec2" {
+#   ami                     = "ami-0c1d144c8fdd8d690"
+#   instance_type           = "t2.micro"
+#   vpc_security_group_ids  = [var.sg]
 
-  provisioner "remote-exec" {
-    connection {
-      type     = "ssh"
-      user     = "centos"
-      password = "DevOps321"
-      host     = self.public_ip         # self. will only work if it's inside the resource ; If not, we need to use aws_instance.my-ec2.public_ip
-      }
-      inline = [
-          "ansible-pull -U https://github.com/b52-clouddevops/ansible.git -e COMPONENT=mongodb -e ENV=dev robot-pull.yml"
-        ]
-    }
-}
+#   provisioner "remote-exec" {
+#     connection {
+#       type     = "ssh"
+#       user     = "centos"
+#       password = "DevOps321"
+#       host     = self.public_ip         # self. will only work if it's inside the resource ; If not, we need to use aws_instance.my-ec2.public_ip
+#       }
+#       inline = [
+#           "ansible-pull -U https://github.com/b52-clouddevops/ansible.git -e COMPONENT=mongodb -e ENV=dev robot-pull.yml"
+#         ]
+#     }
+# }
 
-# Before you use the variable from the root module, ensure you declare an empty variable.
-variable "sg" {}
+# # Before you use the variable from the root module, ensure you declare an empty variable.
+# variable "sg" {}
 
 
-output "public_ip_address" {
-    value = aws_instance.my-ec2.public_ip
-}
+# output "public_ip_address" {
+#     value = aws_instance.my-ec2.public_ip
+# }
 
 
 # Patching of your EC2 & AMI has to be taken care as a part of the monthly maintenance.
@@ -36,31 +36,31 @@ output "public_ip_address" {
 
 
 
-# provider "aws" {
-#   region = "us-east-1"
-# }
+provider "aws" {
+  region = "us-east-1"
+}
 
-# variable "sg" {}
+variable "sg" {}
 
-# resource "aws_instance" "my-ec2" {
-#   ami = "ami-0c1d144c8fdd8d690"
-#   instance_type = "t2.micro"
-#   vpc_security_group_ids = [var.sg]
-#   provisioner "remote-exec" {
-#     connection {
-#       type     = "ssh"
-#       user     = "centos"
-#       password = "DevOps321"
-#       host     = self.public_ip
-#     }
-#     inline = [
-#         "ls -ltr",
-#         "ansible-pull -U https://github.com/lolitaad1989/ansible.git -e COMPONENT=mongodb -e ENV=dev robot-pull.yml"
-#       ]
-#   }
-# }
+resource "aws_instance" "my-ec2" {
+  ami = "ami-0e3aeafe193bbdd4a"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [var.sg]
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = self.public_ip
+    }
+    inline = [
+        "ls -ltr",
+        "ansible-pull -U https://github.com/lolitaad1989/ansible.git -e COMPONENT=mongodb -e ENV=dev robot-pull.yml"
+      ]
+  }
+}
 
 
-# output "public_dins_output" {
-#   value = aws_instance.my-ec2.public_dns
-# }
+output "public_dins_output" {
+  value = aws_instance.my-ec2.public_dns
+}
