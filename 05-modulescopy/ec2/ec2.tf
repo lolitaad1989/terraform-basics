be15e5b2593c2183a62a51908ecbcadc69a9c18c
lolitaad989 <lolitaad19@gmail.com>
2023-02-16 00:30:21 +0530

    terraform basics

--- 05-modulescopy/ec2/ec2.tf
+++ 05-modulescopy/ec2/ec2.tf
@@ -1,6 +1,6 @@
 # creates ec2 instance
 resource "aws_instance" "my-ec2" {
-  ami                     = ami-0c1d144c8fdd8d690
+  ami                     = "ami-0c1d144c8fdd8d690"
   instance_type           = "t2.micro"
   vpc_security_group_ids  = [var.sg]
 
