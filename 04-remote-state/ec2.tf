7af28c5cbdfd30654eaaa6bfb7c599817fb04e06
lolitaad989 <lolitaad19@gmail.com>
2023-02-15 11:30:46 +0530

    terraform basics

--- 04-remote-state/ec2.tf
+++ 04-remote-state/ec2.tf
@@ -3,6 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my-ec2" {
+    count = 3
     ami = "ami-0c1d144c8fdd8d690"
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.allow_ssh.id]
@@ -12,6 +13,6 @@ resource "aws_instance" "my-ec2" {
   }
 }
 
-output "public_dins_output" {
-  value = aws_instance.my-ec2.public_dns
-}
+# output "public_dins_output" {
+#   value = aws_instance.my-ec2.public_dns
+# }
